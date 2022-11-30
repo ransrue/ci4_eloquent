52a89b94d680da1672d0006ec65a9e4f7c18861b
ransrue <wismateduh@gmail.com>
2022-11-30 11:27:42 +0700

    Update Home.php

--- Controllers/Home.php
+++ Controllers/Home.php
@@ -2,16 +2,25 @@
 
 namespace App\Controllers;
 
-use App\Models\Copy;
-use Illuminate\Database\Capsule\Manager as DB;
-
-use CodeIgniter\CodeIgniter;
+use App\Models\User;
 
 class Home extends BaseController
 {
     public function index()
     {
-        $sql = Copy::first();
-        echo $sql;
+        $sql = User::firstOrNew(array('nama' => 'ransrue'));
+        $sql->nama = 'ransrue';
+        $sql->save();
+
+        $sql = User::find(1);
+        echo json_encode($sql);
+        echo '<br>';
+        
+        $sql = User::where('nama', 'buroq');
+        $sql->update(array('nama' => 'buroq.app'));
+
+        $sql = User::get();
+        echo json_encode($sql);
+        echo '<br>';
     }
 }
